fce81c4e63c74f49028602b42ea91ef0a08a4d2b
Steffen <steffen@indigo-nms.eu>
2020-09-24 14:58:34 +0200

    Restructure script and add subscription cancellation

--- index.ts
+++ index.ts
@@ -2,33 +2,60 @@ import type { Request, Response } from 'express'
 import { JWT } from 'google-auth-library'
 import { google } from 'googleapis'
 import { request } from 'https'
-import type { Message, Opts, Telegram, Update, User } from 'typegram'
+import type {
+    InlineKeyboardMarkup,
+    Message,
+    Opts,
+    Telegram,
+    Update,
+    User,
+} from 'typegram'
 
 const GOOGLE_AUTH_SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 const botToken = process.env.BOT_TOKEN ?? ''
 const sheetId = process.env.SHEET_ID ?? ''
-const insiderChatId = process.env.INSIDER_CHAT_ID ?? -1
+const insiderChatId = parseInt(process.env.INSIDER_CHAT_ID ?? '', 10) ?? -1
 
 if (!botToken || !sheetId || insiderChatId === -1) {
     throw new Error('Env var missing!')
 }
 
 // quickly implement Telegram integration to reduce the number of dependencies
-function apiCall<M extends keyof Telegram>(method: M, payload: Opts<M>): void {
+function apiCall<M extends keyof Telegram>(
+    method: M,
+    payload: Opts<M>
+): Promise<ReturnType<Telegram[M]>> {
+    if ('reply_markup' in payload) {
+        // @ts-expect-error: the types are inconsistent with the API so that they can properly reflect the structure of the JSON
+        payload.reply_markup = JSON.stringify(payload.reply_markup)
+    }
     const data = JSON.stringify(payload)
-    const req = request({
-        hostname: 'api.telegram.org',
-        path: '/bot' + botToken + '/' + method,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length,
-        },
+    return new Promise((resolve, reject) => {
+        const req = request(
+            {
+                hostname: 'api.telegram.org',
+                path: '/bot' + botToken + '/' + method,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length,
+                },
+            },
+            res => {
+                let data = ''
+                res.on('error', e => (console.error(e), reject(e)))
+                res.on('data', chunk => (data += chunk))
+                res.on('end', () => {
+                    const r = JSON.parse(data)
+                    if (!r.ok) reject(r.description)
+                    else resolve(r.result)
+                })
+            }
+        )
+        req.write(data)
+        req.end()
     })
-    req.on('error', e => console.log(e))
-    req.write(data)
-    req.end()
 }
 
 // quickly implement auth procedure via google-auth-library because googleapis is just way too complicated
@@ -38,7 +65,7 @@ async function getSheetToken(): Promise<string | null | undefined> {
         const email = process.env.SHEET_CLIENT_EMAIL
         const key = process.env.SHEET_PRIVATE_KEY
         if (!email || !key) {
-            console.log('No sheet auth set!')
+            console.error('No sheet auth set!')
             return undefined
         }
         jwt = new JWT({ email, key, scopes: GOOGLE_AUTH_SCOPES })
@@ -49,11 +76,10 @@ async function getSheetToken(): Promise<string | null | undefined> {
 
 // [[[ BOT CLOUD FUNCTION ]]]
 
-type SpreadsheetRow = [string, 'TRUE' | 'FALSE', number | undefined]
+type SpreadsheetRow = [string, 'TRUE' | 'FALSE', string | undefined]
 
 export async function bot(req: Request, res: Response): Promise<void> {
     const update: Update = req.body
-    console.log('UPDATE DATA', update)
     if ('message' in update) {
         const message = update.message
         if (
@@ -62,8 +88,11 @@ export async function bot(req: Request, res: Response): Promise<void> {
             message.text.startsWith('/start')
         ) {
             await newUser(message)
-        } else if ('new_chat_members' in message) {
-            verifyMembers(message)
+        } else if (
+            message.chat.id === insiderChatId &&
+            'new_chat_members' in message
+        ) {
+            await verifyMembers(message)
         }
     }
     res.end()
@@ -76,7 +105,7 @@ async function newUser(message: Message.TextMessage): Promise<void> {
 
     const token = await getSheetToken()
     if (!token) {
-        apiCall('sendMessage', {
+        await apiCall('sendMessage', {
             chat_id,
             text: 'Something went wrong on our end, sorry!',
         })
@@ -91,7 +120,7 @@ async function newUser(message: Message.TextMessage): Promise<void> {
     })
     const emails = range.data.values as SpreadsheetRow[] | undefined
     if (!emails) {
-        apiCall('sendMessage', {
+        await apiCall('sendMessage', {
             chat_id,
             text: 'Something went wrong on our end, sorry!',
         })
@@ -104,7 +133,7 @@ async function newUser(message: Message.TextMessage): Promise<void> {
     const index = emails.findIndex(row => row[0] === email && row[1] === 'TRUE')
 
     if (index === -1) {
-        apiCall('sendMessage', {
+        await apiCall('sendMessage', {
             chat_id,
             text: "Sorry! You're out!",
         })
@@ -112,10 +141,12 @@ async function newUser(message: Message.TextMessage): Promise<void> {
     }
 
     const row = emails[index]
+    const reply_markup = await getInviteReplyMarkup()
     if (row[2]) {
-        apiCall('sendMessage', {
+        await apiCall('sendMessage', {
             chat_id,
             text: "You're already registered!",
+            reply_markup,
         })
     } else {
         const range = 'C' + (index + 2)
@@ -130,9 +161,10 @@ async function newUser(message: Message.TextMessage): Promise<void> {
                 values: [[chat_id]],
             },
         })
-        apiCall('sendMessage', {
+        await apiCall('sendMessage', {
             chat_id,
             text: "Welcome! You're in!",
+            reply_markup,
         })
     }
 }
@@ -140,13 +172,12 @@ async function newUser(message: Message.TextMessage): Promise<void> {
 // +++ New user joins insider chat +++
 async function verifyMembers(
     message: Message.NewChatMembersMessage
-): Promise<Promise<void>> {
-    const chat_id = message.chat.id
+): Promise<void> {
     const s = google.sheets('v4').spreadsheets
     const token = await getSheetToken()
     if (!token) {
-        apiCall('sendMessage', {
-            chat_id,
+        await apiCall('sendMessage', {
+            chat_id: insiderChatId,
             text: 'Something went wrong on our end, sorry!',
         })
         console.error('Could not authenticate at Google Sheets API, no token!')
@@ -160,38 +191,46 @@ async function verifyMembers(
     })
     const emails = range.data.values as SpreadsheetRow[] | undefined
     if (!emails) {
-        apiCall('sendMessage', {
-            chat_id,
+        await apiCall('sendMessage', {
+            chat_id: insiderChatId,
             text: 'Something went wrong on our end, sorry!',
         })
         console.error('Could not read emails from Google Sheets API!')
         return
     }
 
-    message.new_chat_members.forEach((newMember: User) =>
-        verifyMember(chat_id, emails, newMember)
+    await Promise.all(
+        message.new_chat_members.map((newMember: User) =>
+            verifyMember(emails, newMember)
+        )
     )
 }
 
-function verifyMember(
-    chat_id: number,
+async function verifyMember(
     emails: SpreadsheetRow[],
     newMember: User
-): void {
+): Promise<void> {
     const index = emails.findIndex(
-        row => row[2] === newMember.id && row[1] === 'TRUE'
+        row =>
+            row[2] !== undefined &&
+            parseInt(row[2], 10) === newMember.id &&
+            row[1] === 'TRUE'
     )
     if (index === -1) {
-        apiCall('kickChatMember', {
-            chat_id,
+        await apiCall('kickChatMember', {
+            chat_id: insiderChatId,
             user_id: newMember.id,
         })
-        apiCall('sendMessage', {
-            chat_id,
-            text:
-                'I just had to kick an unwanted user. (Admins can check the action log.)',
-        })
     }
+}
+
+async function getInviteReplyMarkup(): Promise<InlineKeyboardMarkup> {
+    const chat = await apiCall('getChat', { chat_id: insiderChatId })
+    const url =
+        'invite_link' in chat && chat.invite_link !== undefined
+            ? chat.invite_link
+            : await apiCall('exportChatInviteLink', { chat_id: insiderChatId })
+    return { inline_keyboard: [[{ text: 'GET INSIGHTS', url }]] }
 }
 
 // [[[ CANCEL SUBSCRIPTION CLOUD FUNCTION ]]]
@@ -222,10 +261,25 @@ export async function cancelSubscription(
 
     const index = emails.findIndex(row => row[0] === email && row[1] === 'TRUE')
     if (index !== -1) {
+        const range = 'B' + (index + 2)
+        await s.values.update({
+            spreadsheetId: sheetId,
+            range,
+            valueInputOption: 'USER_ENTERED',
+            oauth_token: token,
+            requestBody: {
+                range,
+                majorDimension: 'ROWS',
+                values: [['FALSE']],
+            },
+        })
         const row = emails[index]
-        const user_id = row[2]
-        if (user_id) {
-            apiCall('kickChatMember', { chat_id: insiderChatId, user_id })
+        const id = row[2]
+        if (id) {
+            await apiCall('kickChatMember', {
+                chat_id: insiderChatId,
+                user_id: parseInt(id, 10),
+            })
         }
     }
 
