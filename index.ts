5708245b8c153332afa263e0c3c091041c60d157
Steffen <steffen@indigo-nms.eu>
2020-10-07 18:11:51 +0200

    Cosmetic change

--- index.ts
+++ index.ts
@@ -78,11 +78,9 @@ async function getSheetToken(): Promise<string | null | undefined> {
 
 export async function register(req: Request, res: Response): Promise<void> {
     const { email } = req.query
-    const emailCode = Buffer.from(email?.toString() ?? '', 'ascii').toString(
-        'base64'
-    )
+    const emailString = email?.toString() ?? ''
+    const emailCode = Buffer.from(emailString, 'ascii').toString('base64')
     const me = await apiCall('getMe', {})
-    console.log(email, emailCode)
     res.redirect('https://telegram.me/' + me.username + '?start=' + emailCode)
 }
 
